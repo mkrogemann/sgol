27afa2107a8ee2238b80f4ba036b44a706f1816b
Markus Krogemann <markus@krogemann.de>
2013-12-22 03:37:41 +0100

    add Runner

---
+++ src/main/scala/conway/Runner.scala
@@ -0,0 +1,15 @@
+package conway
+
+object Runner extends App {
+
+  val width  = 100
+  val height = 50
+  val generations = 5000
+  val game = new Game(width,height)
+  var current_generation = game.randomize
+  GamePrinter(width, height, current_generation)
+  for (gen <- 1 to generations) {
+    current_generation = game.evolve(current_generation)
+    GamePrinter(width, height, current_generation)
+  }
+}
