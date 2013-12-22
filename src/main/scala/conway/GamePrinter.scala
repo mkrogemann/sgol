d83d7d35d56cf93485ed40bfd8a0f80c6519fac7
Markus Krogemann <markus@krogemann.de>
2013-12-22 04:12:32 +0100

    slow it down

--- src/main/scala/conway/GamePrinter.scala
+++ src/main/scala/conway/GamePrinter.scala
@@ -2,14 +2,16 @@ package conway
 
 object GamePrinter {
   def apply(width: Int, height: Int, state: Map[(Int,Int), Boolean]): Unit = {
-    for (x <- 1 to width) {
-      for (y <- 1 to height) {
+    for (y <- 1 to height) {
+      for (x <- 1 to width) {
           state.get((x,y)) match {
           case Some(true) => print('o')
           case _ => print(' ')
         }
       }
-      println
+      println()
     }
+    Thread.sleep(100)
+    print(27.toChar + "[2J")
   }
 }
--- src/main/scala/conway/Runner.scala
+++ src/main/scala/conway/Runner.scala
@@ -2,12 +2,14 @@ package conway
 
 object Runner extends App {
 
-  val width  = 100
+  val width  = 120
   val height = 50
   val generations = 5000
   val game = new Game(width,height)
+
   var current_generation = game.randomize
   GamePrinter(width, height, current_generation)
+
   for (gen <- 1 to generations) {
     current_generation = game.evolve(current_generation)
     GamePrinter(width, height, current_generation)
