7e52a9a3535f8059681ac31af6ffacdd6022195f
Markus Krogemann <markus@krogemann.de>
2013-12-25 00:10:13 +0100

    formatting

--- src/main/scala/conway/GamePrinter.scala
+++ src/main/scala/conway/GamePrinter.scala
@@ -1,10 +1,10 @@
 package conway
 
 object GamePrinter {
-  def apply(width: Int, height: Int, state: Map[(Int,Int), Boolean]): Unit = {
+  def apply(width: Int, height: Int, state: Map[(Int, Int), Boolean]): Unit = {
     for (y <- 1 to height) {
       for (x <- 1 to width) {
-          state.get((x,y)) match {
+        state.get((x, y)) match {
           case Some(true) => print('o')
           case _ => print(' ')
         }
