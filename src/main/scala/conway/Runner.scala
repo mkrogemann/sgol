6a6eccf86fba547a2ad554b100d58abb270a37e6
Markus Krogemann <markus@krogemann.de>
2014-02-25 22:23:24 +0100

    set game width to my very personal terminal width

--- src/main/scala/conway/Runner.scala
+++ src/main/scala/conway/Runner.scala
@@ -2,7 +2,7 @@ package conway
 
 object Runner extends App {
 
-  val width  = 120
+  val width  = 170
   val height = 50
   val generations = 5000
   val game = new Game(width,height)
@@ -12,7 +12,7 @@ object Runner extends App {
 
   for (gen <- 1 to generations) {
     current_generation = game.evolve(current_generation)
-    print(27.toChar + "[2J")
+    print(27.toChar + "[2J") /* clear screen */
     GamePrinter(width, height, current_generation)
     println(s"Generation #$gen")
     Thread.sleep(100)
