2b26c45f3cc4100c557d523c0eaaacf33dd6ec59
Markus Krogemann <markus@krogemann.de>
2014-11-04 23:53:43 +0100

    more shorter

--- src/main/scala/conway/Game.scala
+++ src/main/scala/conway/Game.scala
@@ -4,31 +4,30 @@ import scala.util.Random
 
 class Game(val width: Int, val height: Int) {
 
-  def randomize = {
-    var random_state = scala.collection.mutable.Map[(Int, Int), Boolean]()
-    for (x <- 1 to this.width) {
-      for (y <- 1 to this.height) {
-        random_state += (x, y) -> Random.nextBoolean()
-      }
+    def randomize = {
+        var random_state = scala.collection.mutable.Map[(Int, Int), Boolean]()
+        for (x <- 1 to this.width) {
+            for (y <- 1 to this.height) {
+                random_state += (x, y) -> Random.nextBoolean()
+            }
+        }
+        random_state.toMap
     }
-    random_state.toMap
-  }
 
-  def evolve(current_gen: Map[(Int, Int), Boolean]): Map[(Int, Int), Boolean] = {
-    current_gen map { case (k, v) => k -> Rules(neighbors(current_gen, k._1, k._2), alive = v) }
-  }
+    def evolve(current_gen: Map[(Int, Int), Boolean]): Map[(Int, Int), Boolean] = {
+        current_gen map { case (k, v) => k -> Rules(neighbors(current_gen, k._1, k._2), alive = v)}
+    }
 
-  def neighbors(current_gen: Map[(Int, Int), Boolean], x: Int, y: Int): Int = {
-    def alive(x: Int, y: Int): Boolean = current_gen.get(wrapOrGet(x, y)) exists (v => v)
-    List(
-      (x - 1, y - 1), (x, y - 1), (x + 1, y - 1),
-      (x - 1, y), (x + 1, y),
-      (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)).count(t => alive(t._1, t._2))
-  }
+    def neighbors(current_gen: Map[(Int, Int), Boolean], x: Int, y: Int): Int = {
+        def alive(x: Int, y: Int): Boolean = current_gen.get(wrapOrGet(x, y)) exists (v => v)
+        List(
+            (x - 1, y - 1), (x, y - 1), (x + 1, y - 1),
+            (x - 1, y), (x + 1, y),
+            (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)).count(t => alive(t._1, t._2))
+    }
 
-  private def wrapOrGet(x: Int,y: Int): (Int, Int) = {
-    val wrapped_x = if (x < 1) this.width else if (x > this.width) 1 else x
-    val wrapped_y = if (y < 1) this.height else if (y > this.height) 1 else y
-    (wrapped_x, wrapped_y)
-  }
+    private def wrapOrGet(x: Int, y: Int): (Int, Int) = Tuple2(
+        if (x < 1) this.width else if (x > this.width) 1 else x,
+        if (y < 1) this.height else if (y > this.height) 1 else y
+    )
 }
