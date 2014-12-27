5e3634cf72b42c67b1c7d95238ed787757e4e5c4
Markus Krogemann <markus@krogemann.de>
2014-12-27 19:56:20 +0100

    small change in syntax - removes a couple of warnings

--- src/test/scala/conway/RulesSpec.scala
+++ src/test/scala/conway/RulesSpec.scala
@@ -3,28 +3,23 @@ package conway
 import org.scalatest.{FunSpec, Matchers}
 
 class RulesSpec extends FunSpec with Matchers {
-
   describe("Rules") {
-
     describe("apply") {
-
       it ("returns true for three live neighbors") {
-        Rules(neighbors = 3, alive = false) should be(true)
+        Rules(neighbors = 3, alive = false) shouldBe true
       }
 
       it ("returns true if Cell is alive and has two live neighbors") {
-        Rules(neighbors = 2, alive = true) should be(true)
+        Rules(neighbors = 2, alive = true) shouldBe true
       }
 
       it ("returns false for more than three live neighbors") {
-        Rules(neighbors = 4, alive = false) should be(false)
+        Rules(neighbors = 4, alive = false) shouldBe false
       }
 
       it ("returns false for less than two live neighbors") {
-        Rules(neighbors = 1, alive = false) should be(false)
+        Rules(neighbors = 1, alive = false) shouldBe false
       }
     }
-
   }
-
 }
